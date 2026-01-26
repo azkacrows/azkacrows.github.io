a02df609305f9b32dc8a32561853507825284eea
azkacrows <azkacrows@gmail.com>
2026-01-26 15:08:55 +0700

    Updates

---
+++ _next/static/qwJV448NEeoisECgBH2_G/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fcertificates","\u002F[locale]\u002Fexperiences","\u002F[locale]\u002Fprojects"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
